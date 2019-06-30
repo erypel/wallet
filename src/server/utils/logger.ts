ce2397daaf480be563cae6729fc61df8fe363456
Evan <evan.rypel@gmail.com>
2019-06-30 13:40:35 -0700

    added logger and renamed transactionFlow.ts to flow.ts

---
+++ src/server/utils/logger.ts
@@ -0,0 +1,22 @@
+import { createLogger, format, transports } from 'winston'
+import path from 'path'
+
+//This will log to console. Eventually will want a log file
+const logger = caller => {
+	return createLogger({
+		// levels are:
+		// { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
+		level: 'debug',
+		format: format.combine(
+				format.label({ label: path.basename(caller) }),
+				format.colorize(),
+				format.timestamp({
+					format: 'YYYY-MM-DD HH:mm:ss'
+				}),
+				format.printf(info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`)
+		),
+		transports: [new transports.Console()]
+	})
+}
+
+module.exports = logger;
