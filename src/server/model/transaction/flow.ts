5b6b7e4a781a9f6c285f900daedf701a27591a42
Evan <evan.rypel@gmail.com>
2019-07-01 14:00:24 -0700

    added testing framework and implemented very buggy and rudimentary backend and transaction logic. Considering completely eliminating any server side functionality and running everything on the client side

--- src/server/model/transaction/flow.ts
+++ src/server/model/transaction/flow.ts
@@ -1,4 +1,4 @@
-//const logger = require('../utils/logger')(__filename)
+const logger = require('../utils/logger')(__filename)
 const RippleAPI = require('ripple-lib').RippleAPI
 const api = new RippleAPI({
 	server: 'wss://s.altnet.rippletest.net:51233'
@@ -6,52 +6,52 @@ const api = new RippleAPI({
 
 //TODO these are nested calls and problems can occur if the api connection is severed too early. need to Un-nest
 
-function signTransaction(txJSON, secret){
+export default function signTransaction(txJSON, secret){
 	api.connect().then(() => {
-		//logger.debug('signing transaction')
+		logger.debug('signing transaction')
 		return api.sign(txJSON, secret)
 	}).then(signed => {
-		//logger.debug(signed)
-		//logger.debug('signing done')
+		logger.debug(signed)
+		logger.debug('signing done')
 		submitTransaction(signed.signedTransaction)
 	}).then(() => {
 		return api.disconnect()
 	}).then(() => {
-		//logger.debug('done and disconnected')
+		logger.debug('done and disconnected')
 	}).catch(error => {
-		//logger.error(error)
+		logger.error(error)
 	})
 }
 
 function submitTransaction(signedTransaction){
 	api.connect().then(() => {
-		//logger.debug('submitting transaction')
+		logger.debug('submitting transaction')
 		return api.submit(signedTransaction)
 	}).then(result => {
-		//logger.debug(result)
-		//logger.debug('submitted')
+		logger.debug(result)
+		logger.debug('submitted')
 		verifyTransaction(result.id)
 	}).then(() => {
 		return api.disconnect()
 	}).then(() => {
-		//logger.debug('done and disconnected')
+		logger.debug('done and disconnected')
 	}).catch(error => {
-		//logger.error(error)
+		logger.error(error)
 	})
 }
 
 function verifyTransaction(transactionID){
 	api.connect().then(() => {
-		//logger.debug('verifying transaction')
+		logger.debug('verifying transaction')
 		return api.getTransaction(transactionID)
 	}).then(result => {
-		//logger.debug(result)
-		//logger.debug('verified')
+		logger.debug(result)
+		logger.debug('verified')
 	}).then(() => {
 		return api.disconnect()
 	}).then(() => {
-		//logger.debug('done and disconnected')
+		logger.debug('done and disconnected')
 	}).catch(error => {
-		//logger.error(error)
+		logger.error(error)
 	})
 }
---
+++ src/server/test/transaction/flow.test.ts
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('signTransaction', function() {
+    it('should sign a valid transation', function() {
+      //TODO
+    });
+    
+    it('should throw error for invalid transation', function() {
+        //TODO
+    });
+});
