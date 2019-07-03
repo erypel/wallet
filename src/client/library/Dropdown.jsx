66f4f9c098e5fa46bd08955ca8d0a7f2fecee990
Evan <evan.rypel@gmail.com>
2019-07-03 13:41:05 -0700

    add dropdown component and remove some default react stuff

--- src/client/library/Dropdown.jsx
+++ src/client/library/Dropdown.jsx
@@ -1,7 +1,66 @@
 import React from 'react'
 
-//TODO
-const Dropdown = (props) => {
-    return <label>TODO</label>
+class Dropdown extends React.PureComponent {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            listOpen: false,
+            headerTitle: this.props.title
+        }
+    }
+
+    handleClickOutside = () => {
+        this.hideDropdown()
+    }
+
+    showDropdown = () => {
+        this.setState({
+            listOpen: true
+        })
+        document.addEventListener("click", this.hideDropdown);
+    }
+
+    hideDropdown = () => {
+        this.setState({
+            listOpen: false
+        })
+        document.removeEventListener("click", this.hideDropdown);
+    }
+
+    selectItem = (value) => {
+        if (this.state.headerTitle !== value) {
+            this.setState({
+                headerTitle: value
+            })
+        }
+    }
+
+    renderDropdownData = (item, index) => {
+        const { title } = item
+        return <li key={index} onClick={() => this.selectItem(title)}>
+            {title}
+        </li>
+    }
+
+    render() {
+        const { list } = this.props
+        const { listOpen, headerTitle } = this.state
+        return (<div>
+            <div onClick={this.showDropdown}>
+                {headerTitle}
+                {listOpen
+                    ? <label>^</label>
+                    : <label>v</label>
+                }
+            </div>
+            <div>
+                {listOpen && <ul>
+                    {list.map(this.renderDropdownData)}
+                </ul>}
+            </div>
+        </div>
+        )
+    }
 }
 export default Dropdown
--- src/client/pages/Wallet.jsx
+++ src/client/pages/Wallet.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import Button from '../library/Button'
+import Dropdown from '../library/Dropdown'
 
 export default class Wallet extends React.PureComponent {
+    currencies = [
+        {
+            id: 0,
+            title: 'XRP',
+            selected: false,
+            key: 'currency'
+        },
+        {
+          id: 1,
+          title: 'USD',
+          selected: false,
+          key: 'currency'
+        }
+      ];
+
     render() {
-        return <Button>Click Me!</Button>
+        return (<div>
+                <label>Balance <Dropdown
+                    title="Select currency"
+                    list={this.currencies}
+                /></label>
+                <label>Value<Dropdown
+                    title="Select currency"
+                    list={this.currencies}
+                /></label>
+                <Button buttonText='Send'/>
+                <Button buttonText='Receive'/>
+            </div>);
     }
 }
