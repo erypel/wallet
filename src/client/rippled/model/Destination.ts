8cddea3375b5102bcc64ea7643d49807ba7a09f0
Evan <evan.rypel@gmail.com>
2019-07-10 17:15:37 -0700

    forgot these files

--- src/client/rippled/model/Destination.ts
+++ src/client/rippled/model/Destination.ts
@@ -4,9 +4,9 @@ export default class Destination {
   address: string
   amount: Amount
   tag?: string
-  minAmount: Amount
+  minAmount?: Amount
 
-  constructor(address: string, amount: Amount, minAmount: Amount, tag?: string) {
+  constructor(address: string, amount: Amount, minAmount?: Amount, tag?: string) {
     this.address = address
     this.amount = amount
     this.tag = tag
