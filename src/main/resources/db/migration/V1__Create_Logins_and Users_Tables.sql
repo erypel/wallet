726290b735d2a71ee9b055ef425f0c98b3225f89
Evan <evan.rypel@gmail.com>
2019-07-12 17:25:38 -0700

    added flyway and wrote inital migration to create login and user tables

---
+++ src/main/resources/db/migration/V1__Create_Logins_and Users_Tables.sql
@@ -0,0 +1,17 @@
+create table users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+create table logins (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(20) NOT NULL,
+  password varchar(50) NOT NULL,
+  user int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY fk_logins_user_id (user),
+  CONSTRAINT fk_logins_user_id FOREIGN KEY (user) REFERENCES users (id)
+);
+
