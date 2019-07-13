3b7dadec6fef33ce68f794c18525c1955ca50ac4
Evan <evan.rypel@gmail.com>
2019-07-13 13:57:08 -0700

    add unit test for preparePayment

--- src/client/rippled/test/transaction/payment.test.ts
+++ src/client/rippled/test/transaction/payment.test.ts
@@ -1,22 +1,23 @@
-import { TransactionBuilder } from "../../model/transaction/TransactionBuilder";
-import Source from "../../model/Source";
-import Destination from "../../model/Destination";
-import Amount from "../../model/Amount";
-import Currency from "../../model/Currency";
-import Payment from "../../model/transaction/Payment";
+import { TransactionBuilder } from '../../model/transaction/TransactionBuilder'
+import PreparedTransaction from '../../model/transaction/PreparedTransaction'
+import Source from '../../model/Source'
+import Destination from '../../model/Destination'
+import Amount from '../../model/Amount'
+import Currency from '../../model/Currency'
+import Payment from '../../model/transaction/Payment'
 
 var assert = require('chai').assert
 
 const basicPaymentTransaction = {
     "source": {
-      "address": "source",
-      "amount": {
+      "address": "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59",
+      "maxAmount": {
         "value": "100",
         "currency": "XRP"
       }
     },
     "destination": {
-      "address": "destination",
+      "address": "rpZc4mVfWUif9CRoHRKKcmhu1nx2xktxBo",
       "amount": {
         "value": "100",
         "currency": "XRP"
@@ -24,14 +25,30 @@ const basicPaymentTransaction = {
     }
   }
 
+const expectedTxJSONRegex = /{"TransactionType":"Payment","Account":"r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59","Destination":"rpZc4mVfWUif9CRoHRKKcmhu1nx2xktxBo","Amount":"100000000","Flags":2147483648,"LastLedgerSequence":[0-9]*,"Fee":"[0-9]*","Sequence":1}/
+
+function buildPaymentObject(): Payment {
+  const currency = new Currency("XRP", "$")
+  const amount = new Amount(currency, "100")
+  const source = new Source("r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59", undefined, amount)
+  const destination = new Destination("rpZc4mVfWUif9CRoHRKKcmhu1nx2xktxBo", amount)
+  const builder = new TransactionBuilder(source, destination)
+  return new Payment(builder)
+}
+
 describe('basic payment toJsonObject', function() {
     it('should return a valid JSON object', function() {
-        const currency = new Currency("XRP", "$")
-        const amount = new Amount(currency, "100")
-        const source = new Source("source", amount)
-        const destination = new Destination("destination", amount)
-        const builder = new TransactionBuilder(source, destination)
-        const payment = new Payment(builder)
+        const payment = buildPaymentObject()
         assert.deepEqual(payment.toJsonObject(), basicPaymentTransaction)
     });
 });
+
+describe('prepare payment', function() {
+  it('should return a prepared payment with txJSON and instructions', async function(done) {
+    const payment = buildPaymentObject()
+    const preparedPayment = await payment.send()
+    assert.isTrue(expectedTxJSONRegex.test(preparedPayment.txJSON))
+    assert.isObject(preparedPayment.instructions)
+    done()
+  })
+})
