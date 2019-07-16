f9bae275198c8e19453810204866c23b159870c8
Evan <evan.rypel@gmail.com>
2019-07-16 12:22:03 -0700

    creating what will eventually become pages for login and create user, create Login object, create login form, work on Login and User stores

--- src/client/redux/store/LoginStore.ts
+++ src/client/redux/store/LoginStore.ts
@@ -1,7 +1,71 @@
-function login(username: string, password: string) {
+import { createStore } from 'redux'
+import User from '../../model/User';
+
+const LOGIN_REQUEST = 'USERS_LOGIN_REQUEST'
+const LOGIN_SUCCESS = 'USERS_LOGIN_SUCCESS'
+const LOGIN_FAILURE = 'USERS_LOGIN_FAILURE'
+const LOGOUT = 'USERS_LOGOUT'
+
+interface LoginRequestAction {
+    type: typeof LOGIN_REQUEST
+    payload: User
+}
+
+interface LoginSuccessAction {
+    type: typeof LOGIN_SUCCESS
+    payload: User
+}
+
+interface LoginFailureAction {
+    type: typeof LOGIN_FAILURE
+    payload: Error
+}
+
+interface LogoutAction {
+    type: typeof LOGOUT
+    payload: null
+}
+
+type Actions= LoginRequestAction | LoginSuccessAction | LoginFailureAction | LogoutAction
+
+function reducer(state = {}, action: Actions) {
+    const { type } = action
+    switch (type) {
+        case LOGIN_REQUEST:
+            return {}
+        case LOGIN_SUCCESS:
+            return {}
+        case LOGIN_FAILURE:
+            return {}
+        case LOGOUT:
+            return {}
+        default:
+            return state
+    }
+}
+
+function loginRequestAction(): LoginRequestAction {
+
+}
+
+function loginSuccessAction(): LoginSuccessAction {
+
+}
+
+function loginFailureAction(): LoginFailureAction {
+
+}
+
+function logoutAction(): LogoutAction {
+    return { type: LOGOUT, payload: null}
+}
+
+export const LoginStore = createStore(reducer)
+
+export function login(username: string, password: string) {
     
 }
 
-function logout() {
-    
+export function logout() {
+    LoginStore.dispatch(logoutAction())
 }
--- src/client/redux/store/UserStore.ts
+++ src/client/redux/store/UserStore.ts
@@ -1,5 +1,9 @@
 import User from "../../model/User";
 
+const REGISTER_REQUEST = 'USERS_REGISTER_REQUEST'
+const REGISTER_SUCCESS = 'USERS_REGISTER_SUCCESS'
+const REGISTER_FAILURE = 'USERS_REGISTER_FAILURE'
+
 function createUser(user: User) {
 
 }
