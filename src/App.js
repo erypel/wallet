00a3c7568adfb9d788f54df5f2fb723dcda246ee
Evan <evan.rypel@gmail.com>
2019-07-17 13:18:37 -0700

    intalled route library, but will tackle routing in the next pull request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Login from './client/pages/Login'
-import { Provider } from 'react-redux';
-import { LoginStore } from './client/redux/store/LoginStore';
+import Register from './client/pages/Register'
+import Wallet from './client/pages/Wallet'
+import { Route, Router } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { LoginStore } from './client/redux/store/LoginStore'
 
 class App extends React.PureComponent {
 
   render() {
     return <Provider store={LoginStore}>
-        <div className="App">
+        <div classname="App">
+          {/* <Router>
+            <Route path="/" component={Login}/>
+            <Route path='/register' component={Register}/>
+            <Route path='/wallet' component={Wallet}/>
+          </Router> */}
           <Login/>
         </div>
       </Provider>
