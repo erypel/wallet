ec77502d7c2b4335b57ccc3264f5fc41c5c9efe3
Evan <evan.rypel@gmail.com>
2019-07-17 15:05:13 -0700

    added support for nonexistant routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PrivateRoute from './client/library/PrivateRoute'
 import Login from './client/pages/Login'
 import Register from './client/pages/Register'
 import Wallet from './client/pages/Wallet'
-import { Route, Router } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { LoginStore } from './client/redux/store/LoginStore'
 
@@ -13,10 +13,13 @@ class App extends React.PureComponent {
   render() {
     return <Provider store={LoginStore}>
         <div classname="App">
-          <PrivateRoute path="/" exact component={Wallet}/>
-          <Route path="/login" exact component={Login}/>
-          <Route path='/register' component={Register}/>
-          <PrivateRoute path='/wallet' component={Wallet}/>
+          <Switch>
+            <PrivateRoute path="/" exact component={Wallet}/>
+            <Route path="/login" exact component={Login}/>
+            <Route path='/register' component={Register}/>
+            <PrivateRoute path='/wallet' component={Wallet}/>
+            <Route render={() => (<div> Sorry, this page does not exist. </div>)} />
+          </Switch>
         </div>
       </Provider>
   }
