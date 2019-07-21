586ad303637ddbd543ecbb010347883614bd3eba
Evan <evan.rypel@gmail.com>
2019-07-21 11:18:41 -0700

    create wallet data layer

---
+++ src/main/kotlin/dao/WalletDao.kt
@@ -0,0 +1,27 @@
+package dao
+
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.IntIdTable
+
+data class Wallet(
+        var privateKey: String,
+        var publicKey: String,
+        var id: Int,
+        var userId: Int
+)
+
+class WalletDao(id: EntityID<Int>): IntEntity(id) {
+    companion object : IntEntityClass<WalletDao>(Wallets)
+
+    var privateKey by Wallets.privateKey
+    var publicKey by Wallets.publicKey
+    var userId by User referencedOn Users.id
+}
+
+object Wallets: IntIdTable() {
+    val privateKey = varchar("privateKey", 511)
+    val publicKey = varchar("publicKey", 511)
+    val userId = integer("userId").references(Users.id)
+}
