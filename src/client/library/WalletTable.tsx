048c3c56f60b12a3913363e83b771ca09a654cff
Evan <evan.rypel@gmail.com>
2019-07-22 12:16:15 -0700

    wallet store synced up on initial render!

--- src/client/library/WalletTable.tsx
+++ src/client/library/WalletTable.tsx
@@ -6,50 +6,15 @@ import { Table, Thead, Th, Tr, Tbody, Td } from './Table';
 import GenerateWalletButton from './GenerateWalletButton';
 import { LoginStore } from '../redux/store/LoginStore';
 
-interface Props {
-    store?: {
-        wallets: Wallet[]
-    }
-}
-
 const mapStateToProps = (state: AppState) => {
     return {wallets: state.wallets}
 }
 
-const ConnectedTable = (store: { wallets: Wallet[] }) => {
-        
-        if (!store || !store.wallets || store.wallets.length < 1){
-            return <GenerateWalletButton/>
-        }
-        const { wallets } = store
-        return <Table>
-            <Thead>
-                <Tr>
-                    <Th>Public Key</Th>
-                    <Th>Private Key</Th>
-                    <Th>User ID</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                {wallets.map(wallet => (<Tr>
-                        <Td>{wallet.publicKey}</Td>
-                        <Td>{wallet.privateKey}</Td>
-                        <Td>{wallet.userId}</Td>
-                    </Tr>)
-                )}
-                <Tr colSpan={4}><GenerateWalletButton/></Tr>
-            </Tbody>
-        </Table>
+interface Props {
+    wallets: Wallet[] 
 }
 
-// class ConnectedTable extends React.PureComponent<Props> {
-//     componentWillMount() {
-//         ws.load(LoginStore.getState().user!!.id!!)
-//     }
-
-//     render() {
-//         const { store } = this.props
-        
+// const ConnectedTable = (store: { wallets: Wallet[] }) => {
 //         if (!store || !store.wallets || store.wallets.length < 1){
 //             return <GenerateWalletButton/>
 //         }
@@ -72,8 +37,39 @@ const ConnectedTable = (store: { wallets: Wallet[] }) => {
 //                 <Tr colSpan={4}><GenerateWalletButton/></Tr>
 //             </Tbody>
 //         </Table>
-//     }
 // }
+
+class ConnectedTable extends React.PureComponent<Props> {
+    componentWillMount() {
+        ws.load(LoginStore.getState().user!!.id!!)
+    }
+
+    render() {
+        const { wallets } = this.props
+        
+        if ( !wallets ||wallets.length < 1){
+            return <GenerateWalletButton/>
+        }
+        return <Table>
+            <Thead>
+                <Tr>
+                    <Th>Public Key</Th>
+                    <Th>Private Key</Th>
+                    <Th>User ID</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {wallets.map(wallet => (<Tr>
+                        <Td>{wallet.publicKey}</Td>
+                        <Td>{wallet.privateKey}</Td>
+                        <Td>{wallet.userId}</Td>
+                    </Tr>)
+                )}
+                <Tr colSpan={4}><GenerateWalletButton/></Tr>
+            </Tbody>
+        </Table>
+    }
+}
 
 const WalletTable = connect(mapStateToProps)(ConnectedTable)
 
