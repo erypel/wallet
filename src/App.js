774eede7a123fc49bb23e1f5c6a7b9e7209de72a
Evan <evan.rypel@gmail.com>
2019-07-22 13:25:20 -0700

    add route switch to handle non existant client side routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PrivateRoute from './client/library/PrivateRoute'
 import Login from './client/pages/Login'
 import Register from './client/pages/Register'
 import Wallet from './client/pages/Wallet'
-import { Route, Router } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { LoginStore } from './client/redux/store/LoginStore'
 import { history } from './client/utils/history'
@@ -16,10 +16,13 @@ class App extends React.PureComponent {
         <div className="App">
           <Router history={history}>
             <div>
-              <PrivateRoute path="/" exact component={Wallet}/>
-              <Route path="/login" exact component={Login}/>
-              <Route path='/register' component={Register}/>
-              <PrivateRoute path='/wallet' component={Wallet}/>
+              <Switch>
+                <PrivateRoute path="/" exact component={Wallet}/>
+                <Route path="/login" exact component={Login}/>
+                <Route path='/register' component={Register}/>
+                <PrivateRoute path='/wallet' component={Wallet}/>
+                <Route render={() => (<div> Sorry, this page does not exist. </div>)} />
+              </Switch>
             </div>
           </Router>
         </div>
