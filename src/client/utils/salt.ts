f043da9eb2a35c40424bedd0638dd988151375ae
Evan <evan.rypel@gmail.com>
2019-07-24 17:34:39 -0700

    salt

--- src/client/utils/salt.ts
+++ src/client/utils/salt.ts
@@ -1,12 +1,12 @@
 import crypto from 'crypto'
 
 export default function salt(
-	password: string,
+	value: string,
 	salt: string = randomString()
 ) {
 	const hash = crypto
 	.createHmac('sha512', salt)
-	.update(password)
+	.update(value)
 	return {
 		salt,
 		hash: hash.digest('hex')
