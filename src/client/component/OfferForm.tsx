6f8c1de0f406763dd7096c77d498bc3726411fe9
Evan <evan.rypel@gmail.com>
2019-08-05 13:36:31 -0700

    order form with basic fields; commiting before creating binary switch

---
+++ src/client/component/OfferForm.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Tabs from '../container/Tabs'
+import Input from '../library/Input'
+
+interface Props {
+    bidCurrency: string
+    askCurrency: string
+}
+
+interface State {
+    amount: number
+    limitPrice: number
+    stopPrice: number
+}
+
+class OfferForm extends React.PureComponent<Props, State> {
+    constructor(props: Props) {
+        super(props)
+
+        this.state = {
+            amount: 0.00,
+            limitPrice: 0.00,
+            stopPrice: 0.00
+        }
+    }
+
+    render() {
+        const { amount, limitPrice, stopPrice } = this.state
+        const { bidCurrency, askCurrency } = this.props
+        return <form>
+            <Tabs>
+                <div data-label='market'>
+                    <label>
+                        Amount
+                        <Input id='amount' type='number' value={amount}/> {bidCurrency}
+                    </label>
+                </div>
+                <div data-label='limit'>
+                    <label>
+                        Amount
+                        <Input id='amount' type='number' value={amount}/> {askCurrency}
+                    </label>
+                    <br/>
+                    <label>
+                        Limit Price
+                        <Input id='limitPrice' type='number' value={limitPrice}/> {bidCurrency}
+                    </label>
+                </div>
+                <div data-label='stop'>
+                    <label>
+                        Stop Price
+                        <Input id='amount' type='number' value={stopPrice}/> {bidCurrency}
+                    </label>
+                    <br/>
+                    <label>
+                        Amount
+                        <Input id='amount' type='number' value={amount}/> {askCurrency}
+                    </label>
+                    <br/>
+                    <label>
+                        Limit Price
+                        <Input id='limitPrice' type='number' value={limitPrice}/> {bidCurrency}
+                    </label>
+                </div>
+            </Tabs>
+        </form>
+    }
+}
+
+export default OfferForm
