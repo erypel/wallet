b9bf930453470752d930e938f4b5f5a032265eae
Evan <evan.rypel@gmail.com>
2019-08-10 16:10:16 -0700

    display drops as xrp in currency service amount formatter

--- src/client/services/currencyService.ts
+++ src/client/services/currencyService.ts
@@ -1,8 +1,9 @@
 import Amount, { IssuerAmount } from '../xrpl/api/model/Amount'
+import dropsToXrp from '../xrpl/api/utils/dropsToXrp'
 
 function createCurrencyString(amount: Amount | IssuerAmount | string) {
     if(typeof amount === 'string') {
-        return `${amount} XRP`
+        return `${dropsToXrp(amount)} XRP`
     } else {
         return `${amount.value} ${amount.currency}`
     }
