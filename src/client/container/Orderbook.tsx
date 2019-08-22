ebd52d353869a10ab25ca76e559e16717c08045e
Evan <evan.rypel@gmail.com>
2019-08-22 10:42:18 -0700

    endowed the spread with functionality

--- src/client/container/Orderbook.tsx
+++ src/client/container/Orderbook.tsx
@@ -11,14 +11,26 @@ interface Props {
 
 class Orderbook extends React.PureComponent<Props> {
     render() {
-        const { bids, asks } = this.props
+        const { bids, asks, baseCurrency, quoteCurrency } = this.props
         const bidsSize = bids.length > 10 ? 10 : bids.length
         const asksSize = asks.length > 10 ? 10 : asks.length
+        const hasSpread = asksSize !== 0 && bidsSize !== 0
+        var bidSpread = 0
+        var askSpread = 0
+        if (hasSpread) {
+            const lowestBid = bids[bidsSize -1]
+            const lowestBidSpec = lowestBid.specification
+            bidSpread = Number(lowestBidSpec.totalPrice.value) / Number(lowestBidSpec.quantity.value)
+            const highestAsk = asks[0]
+            const highestAskSpec = highestAsk.specification
+            askSpread = Number(highestAskSpec.totalPrice.value) / Number(highestAskSpec.quantity.value)
+        }
         return <div className='orderbook'>
                 <h1>Orderbook</h1>
                 <table>
                     <thead>
                         <tr>
+                            <th></th>
                             <th>MarketSize</th>
                             <th>Price</th>
                         </tr>
@@ -26,18 +38,21 @@ class Orderbook extends React.PureComponent<Props> {
                     <tbody>
                         {bids.slice(0, bidsSize).map((bid: Bid, idx: number) => {
                             return <tr className='green-text' key={idx}>
-                                <td>{bid.specification.quantity.value} {bid.specification.quantity.currency}</td>
-                                <td>{bid.specification.totalPrice.value} {bid.specification.totalPrice.currency}</td>
+                                <td>{idx === 0 ? 'Offers to buy' : ''}</td>
+                                <td>{bid.specification.quantity.value} {baseCurrency}</td>
+                                <td>{bid.specification.totalPrice.value} {quoteCurrency}</td>
                             </tr>
                         })}
-                        <tr>
+                        {hasSpread && <tr>
+                            <td></td>
                             <td>Spread</td>
-                            <td>{'TBD'}</td>
-                        </tr>
+                            <td>{Math.abs(bidSpread - askSpread)} {quoteCurrency}</td>
+                        </tr>}
                         {asks.slice(0, asksSize).map((ask: Ask, idx: number) => {
                             return <tr className='red-text' key={idx}>
-                                <td>{ask.specification.quantity.value} {ask.specification.quantity.currency}</td>
-                                <td>{ask.specification.totalPrice.value} {ask.specification.totalPrice.currency}</td> 
+                                <td>{idx === 0 ? 'Offers to sell' : ''}</td>
+                                <td>{ask.specification.quantity.value} {baseCurrency}</td>
+                                <td>{ask.specification.totalPrice.value} {quoteCurrency}</td> 
                             </tr>
                         })}
                     </tbody>
