20251e803ccfd1e7f86f249a187fed9c6ea50168
Evan <evan.rypel@gmail.com>
2019-08-31 16:05:36 -0700

    create an httpService

--- src/client/services/userService.ts
+++ src/client/services/userService.ts
@@ -3,37 +3,21 @@ import User from '../model/User'
 import salt from '../utils/salt'
 import UserDetail from '../model/UserDetail'
 import Pair from '../model/Pair'
+import { http } from './httpService'
 
 //login store methods
 async function login(login: Login): Promise<Pair | undefined> {
-    return await fetch('http://localhost:7000/user/login/', {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'text/plain'
-        },
-        credentials: 'include',
-        body: JSON.stringify(login),
-        method: 'POST'
-    }).then(async res => {
-        return await res.json()
-    }).catch(() => {
-        return undefined
-    })
+    return await http.post(
+        'http://localhost:7000/user/login/',
+        JSON.stringify(login)
+    )
 }
 
 async function logout(userId: string) {
-    await fetch(`http://localhost:7000/user/logout/${userId}/`, {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        method: 'POST'
-    }).then(response => {
-        console.log(response)
-    }).catch(error => {
-        alert(error)
-    })
+    await http.post(
+        `http://localhost:7000/user/logout/${userId}/`,
+        null
+    )
 }
 
 //user store methods
@@ -47,36 +31,18 @@ async function register(user: User): Promise<User | undefined> {
         password: user.password,
         salt: user.salt
     }
-    await fetch('http://localhost:7000/user/create/', {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        method: 'POST',
-        body: JSON.stringify(newUser)
-    }).then(response => {
-        console.log(response)
-    }).catch(error => {
-        alert(error)
-    })
+    await http.post(
+        'http://localhost:7000/user/create/',
+        JSON.stringify(newUser)
+    )
     return undefined
 }
 
 async function update(detail: UserDetail): Promise<any> {
-    return await fetch('http://localhost:7000/user/update/', {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'text/plain'
-        },
-        credentials: 'include',
-        body: JSON.stringify(detail),
-        method: 'POST'
-    }).then(async res => {
-        return await res.json()
-    }).catch(() => {
-        return undefined
-    })
+    return await http.post(
+        'http://localhost:7000/user/update/',
+        JSON.stringify(detail)
+    )
 }
 
 export const userService = {
--- src/client/services/walletService.ts
+++ src/client/services/walletService.ts
@@ -1,44 +1,21 @@
 import Wallet from '../model/Wallet'
 import { WalletMap } from '../store/wallet/types'
 import { Balance } from '../xrpl/api/model/Balance'
+import { http } from './httpService'
 
 async function create(wallet: Wallet): Promise<Wallet | undefined> {
-    return await fetch('http://localhost:7000/wallet/create', {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        method: 'POST',
-        body: JSON.stringify({
+    return await http.post(
+        'http://localhost:7000/wallet/create', 
+        JSON.stringify({
             privateKey: wallet.privateKey,
             publicKey: wallet.publicKey,
             userId: wallet.userId
         })
-    }).then(async response => {
-        const json = await response.json()
-        console.log(json)
-        return json
-    }).catch(error => {
-        alert(error)
-    })
+    )
 }
 
 async function loadList(userId: string): Promise<WalletMap> {
-    return await fetch(`http://localhost:7000/wallet/user/${userId}`, {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        method: 'GET'
-    }).then(async response => {
-        const json = await response.json()
-        console.log(json)
-        return json
-    }).catch(error => {
-        alert(error)
-    })
+    return await http.get(`http://localhost:7000/wallet/user/${userId}`)
 }
 
 function findBalance(matchCurrency: string, balances: Balance[]): string {
@@ -54,20 +31,7 @@ function findBalance(matchCurrency: string, balances: Balance[]): string {
 }
 
 async function deleteWallet(privateKey: string) {
-    return await fetch(`http://localhost:7000/wallet/${privateKey}`, {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        credentials: 'include',
-        method: 'DELETE'
-    }).then(async response => {
-        const json = await response.json()
-        console.log(json)
-        return json
-    }).catch(error => {
-        alert(error)
-    })
+    return await http.delete(`http://localhost:7000/wallet/${privateKey}`)
 }
 
 export const walletService = {
